d28d53b5f17952de9ef16b8b4e79c5c00d936fb5
itsakash9 <107836152+itsakash9@users.noreply.github.com>
2022-06-20 12:29:25 +0530

    Update SumOfTwoNumbers.java

--- SumOfTwoNumbers.java
+++ SumOfTwoNumbers.java
@@ -2,7 +2,7 @@ public class SumOfTwoNumbers
 {  
 public static void main(String args[])   
 {  
-int n1 = 09, n2 = 99, sum;  
+int n1 = 99, n2 = 999, sum;  
 sum = n1 + n2;  
 System.out.println("The sum of two numbers is: "+sum);  
 }  
