5d6217ae93aead7e28bf8bde8f6fcb3baff72088
itsakash9 <107836152+itsakash9@users.noreply.github.com>
2022-06-20 12:26:40 +0530

    Create SumOfTwoNumbers.java

---
+++ SumOfTwoNumbers.java
@@ -0,0 +1,9 @@
+public class SumOfTwoNumbers  
+{  
+public static void main(String args[])   
+{  
+int n1 = 09, n2 = 99, sum;  
+sum = n1 + n2;  
+System.out.println("The sum of two numbers is: "+sum);  
+}  
+}  
